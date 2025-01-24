f5d49137a6d90a9ea7697be7b1fb75233ec1a1a1
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2025-01-24 16:32:05 +0100

    dry code

--- frontend/src/app/components/nutritionist/nutritionist.component.ts
+++ frontend/src/app/components/nutritionist/nutritionist.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './nutritionists.component.css'
 })
 export class NutritionistsComponent {
-
+  nutritionists = [
+    { name: 'Mike Tyson', image: 'nutritionnist1.png', delay: 0 },
+    { name: 'Ronda Rousey', image: 'nutritionnist2.png', delay: 300 }
+  ];
 }
