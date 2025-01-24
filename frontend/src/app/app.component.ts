d5f77bf219695cf96c8e4d54ad121ae1e9be59b2
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2025-01-24 12:52:20 +0100

    stats animation

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  ngOnInit(): void {
+    AOS.init({
+      duration: 1000, 
+      once: false,     
+    });
+  }
 }
