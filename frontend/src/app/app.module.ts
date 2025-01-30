7ab842f0de2c7222c04b38a4f15c101f3a5eaee7
lamiabelhadj <lamya.bhs@gmail.com>
2025-01-30 15:44:15 +0100

    fixes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,14 +21,15 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NavbarComponent } from './navbar/navbar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
     AppComponent,
       
     SidebarComponent,   
     HomeComponent,   
-    CardComponent, NavbarComponent, DashboardComponent,    
+    CardComponent, NavbarComponent, 
+    // DashboardComponent,    
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -8,18 +8,18 @@ import { Router } from '@angular/router';
 export class HomeComponent {
   cards = [
     { title: 'Dashboard', icon: 'dashboard', route: '/dashboard', image: 'assets/dashboard3.png' }, 
-    { title: 'Classes', icon: 'class', route: '/classes', image: 'assets/classes.png' }, 
-    { title: 'Nutrition', icon: 'restaurant', route: '/nutrition', image: 'assets/nutrition.png' }, 
+    { title: 'Classes', icon: 'class', route: '/classes', image: 'assets/classes.webp' }, 
+    { title: 'Nutrition', icon: 'restaurant', route: '/nutrition', image: 'assets/nutrition.jpg' }, 
 
     { title: 'Performance', icon: 'trending_up', route: '/performance', image: 'assets/performance.png' },
     { title: 'Private Sessions', icon: 'fitness_center', route: '/private-sessions', image: 'assets/private.png' }, 
 
 
-    { title: 'Plannings', icon: 'event', route: '/plannings', image: 'assets/planning2.png' },
-    { title: 'Subscriptions', icon: 'subscriptions', route: '/subscriptions', image: 'assets/subscriptions.png' }, 
-    { title: 'Payments', icon: 'credit_card', route: '/payments', image: 'assets/payment.png' }, 
-    { title: 'Contact Us', icon: 'contact_mail', route: '/contact-us' , image: 'assets/contact.png'}, 
-    { title: 'Help & Support', icon: 'help', route: '/help-support', image: 'assets/support.png' },
+    { title: 'Plannings', icon: 'event', route: '/plannings', image: 'assets/calendar.webp' },
+    { title: 'Subscriptions', icon: 'subscriptions', route: '/subscriptions', image: 'assets/subscriptions.jpg' }, 
+    { title: 'Payments', icon: 'credit_card', route: '/payments', image: 'assets/payment.avif' }, 
+    { title: 'Contact Us', icon: 'contact_mail', route: '/contact-us' , image: 'assets/contact.webp'}, 
+    { title: 'Help & Support', icon: 'help', route: '/help-support', image: 'assets/help.webp' },
  
 
   ];
