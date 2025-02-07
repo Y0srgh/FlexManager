fcd8c25d43363accf8fef9237aa696872448cef6
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2025-02-07 20:42:55 +0100

    RBAC fixed

--- frontend/src/app/pages/membership-page/membership-list/membership-list.component.ts
+++ frontend/src/app/pages/membership-page/membership-list/membership-list.component.ts
@@ -3,53 +3,64 @@ import { MatDialog } from '@angular/material/dialog';
 import { DetailsModalComponent } from './details-dialog/details-modal/details-modal.component';
 import { MembershipService } from 'src/app/services/membership.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-membership',
   templateUrl: './membership-list.component.html',
-  styleUrls: ['./membership-list.component.css']
+  styleUrls: ['./membership-list.component.css'],
 })
 export class MemberShipComponent implements OnInit {
-  subscriptions:any = [];
-  selectedSubscription:any
+  subscriptions: any = [];
+  selectedSubscription: any;
 
   imagesMap: { [key: number]: string } = {
-    1: "assets/bodybuilding.jpg",
-    2: "assets/cardio.jpeg",
-    3: "assets/swiming.jpeg",
-    4: "assets/fullAccees.jpeg",
-    5: "assets/weekendonly.jpeg",
-    6: "assets/morning.jpeg",
-    7: "none"
+    1: 'assets/bodybuilding.jpg',
+    2: 'assets/cardio.jpeg',
+    3: 'assets/swiming.jpeg',
+    4: 'assets/fullAccees.jpeg',
+    5: 'assets/weekendonly.jpeg',
+    6: 'assets/morning.jpeg',
+    7: 'none',
   };
+  currentRole: string = '';
 
-  constructor(private memberShipService: MembershipService,
-    private dialog: MatDialog , private router:Router
+  constructor(
+    private memberShipService: MembershipService,
+    private dialog: MatDialog,
+    private router: Router,
+    private auth: AuthService
   ) {}
 
   ngOnInit(): void {
+    const userRole = this.auth.getCurrentUserRole();
+    this.currentRole = userRole!;
     this.memberShipService.getMemberships().subscribe(
       (data) => {
-        this.subscriptions = data.map(item => ({
-      ...item,
-      image: this.imagesMap[item.id] !== "none" ? this.imagesMap[item.id] : null
-    }));;
-        console.log(this.subscriptions)
+        this.subscriptions = data.map((item) => ({
+          ...item,
+          image:
+            this.imagesMap[item.id] !== 'none' ? this.imagesMap[item.id] : null,
+        }));
+        console.log(this.subscriptions);
       },
       (error) => {
         console.error('Erreur lors du chargement des abonnements', error);
       }
     );
   }
-  openModal(subscription:any){
 
+  isClient(): boolean {
+    return this.currentRole === 'client';
+  }
+
+  openModal(subscription: any) {
     this.dialog.open(DetailsModalComponent, {
-      data:subscription,
-      minWidth:'500px'
+      data: subscription,
+      minWidth: '500px',
     });
   }
-  OnclickPay(){
-    this.router.navigate(["/payment"]);
+  OnclickPay() {
+    this.router.navigate(['/payment']);
   }
-
 }
